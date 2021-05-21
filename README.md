315c7bdd8714c28e31f2506f7cd155ee751addf4
hemu75439 <himanshu75439@gmail.com>
2021-05-21 21:33:04 +0530

    V 1.0

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# marvelapp
+<H1>Marvel App</h1>
+<br>
+<p>This is a simple website that show all the marvel universe characters.</p>
+
+<p>I made this just for fun....</p>
+
+<h2>Specifications: </h2>
+
+<p>I have used Marvel's api to build this website.</p>
+
+<p>If you want to make the same or clone my project: </p>
+
+<p> Just vist : <a href="https://developer.marvel.com">Marvel's Developer's Site</a> and create your own API keys </p>
+
+<p> You will get a <strong>Public Key</strong> and a <strong> Private Key </strong> </p>
+
+<br>
+
+<p>After that you just have to clone this project and add a file in root directory named "cred.js"</p>
+<p>In the file "cred.js" just write the following code: </p>
+
+<p>export const ts_apikey_hash = "ts=YOUR_TS&apikey=YOUR_API_KEY&hash=YOUR_HASH"</p>
+
+<ul>
+    <li>YOUR_TS - It just any number you wish (make sure its +ve integers)</li>
+    <li>YOUR_API_KEY - It is your PUBLIC KEY</li>
+    <li>YOUR_HASH - It is a md5 hash of the sum : (YOUR_TS + YOUR_PUBLIC_KEY + YOUR_PRIVATE_KEY)</li>
+    <li>* Just google search md5 generator</li>
+    <li>For more info check https://developer.marvel.com/documentation/authorization <li>
+<ul>
+
+<p>There you go, you are all set...</p>
---
+++ app.js
@@ -0,0 +1,56 @@
+import {ts_apikey_hash} from './cred.js'
+
+const main = document.querySelector('main')
+var offset = 0
+
+const getMoreBtn = document.querySelector('.get-more button')
+getMoreBtn.addEventListener('click', getMore)
+
+
+function getMore(e) {
+    e.preventDefault()
+
+    offset += 100
+    getItems()
+}
+
+// To get all the characters
+function getItems(){
+    fetch(`http://gateway.marvel.com/v1/public/characters?limit=100&offset=${offset}&${ts_apikey_hash}`)
+    .then(res => res.json())
+    .then(data => updateUI(data))
+    .catch(err => console.log(err))
+}
+
+// To set all the characters on the screen
+function updateUI(data) {
+    setfooter(data.attributionHTML)
+    data.data.results.forEach(d => {
+        let a = document.createElement('a')
+        a.href = d.urls[0].url
+        a.target = "_blank"
+
+        let div = document.createElement('div')
+        div.classList.add('character')
+
+        let img = document.createElement('img')
+        img.src = d.thumbnail.path + "/standard_fantastic." + d.thumbnail.extension
+        img.alt = d.name
+        div.appendChild(img)
+
+        let h2 = document.createElement('h2')
+        h2.innerText = d.name
+        div.appendChild(h2)
+
+        a.appendChild(div)
+        main.appendChild(a)
+    });
+}
+
+function setfooter(footer) {
+    document.querySelector('footer').innerHTML = footer
+}
+
+
+
+getItems()
